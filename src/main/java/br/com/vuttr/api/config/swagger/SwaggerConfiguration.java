b2e0ae356322f0168a289aad85812de4e76fc43e
Savio Carvalho <savio_carvalho@outlook.com>
2020-08-29 13:12:28 -0300

    adicionado documentação via ferrameta swagger (http://localhost:3000/swagger-ui.html)

---
+++ src/main/java/br/com/vuttr/api/config/swagger/SwaggerConfiguration.java
@@ -0,0 +1,51 @@
+package br.com.vuttr.api.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket vuttrApiDocumentation(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.vuttr.api.controller"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .apiInfo(apiInfo())
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Header para token JWT")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(false)
+                        .build()
+                ));
+    }
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Desafio Back-end Bossabox")
+                .description("A aplicação é um simples repositório para gerenciar ferramentas com seus respectivos nomes, links, descrições e tags.")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("Savio Carvalho", "https://github.com/Saviocarval","savio_carvalho@outlook.com"))
+                .build();
+    }
+
+
+}
--- src/main/java/br/com/vuttr/api/controller/ToolsController.java
+++ src/main/java/br/com/vuttr/api/controller/ToolsController.java
@@ -5,6 +5,7 @@ import br.com.vuttr.api.controller.dto.ToolsDto;
 import br.com.vuttr.api.controller.form.ToolsForm;
 import br.com.vuttr.api.model.Tools;
 import br.com.vuttr.api.repository.ToolsRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,6 +26,7 @@ public class ToolsController {
     private ToolsRepository repository;
 
     @GetMapping
+    @ApiOperation( value = "Listar todos as feramenstas e filtar por tag")
     public Page<ToolsDto> listarTodos(@RequestParam(required = false) String tag,Pageable page){
         if(tag == null){
             Page<Tools> tools = repository.findAll(page);
