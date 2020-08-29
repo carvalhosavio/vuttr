d9ecb04fc7587580cce2e3d01d5e779fdd03d43d
Savio Carvalho <savio_carvalho@outlook.com>
2020-08-29 10:57:28 -0300

    first commit

---
+++ src/test/java/br/com/vuttr/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vuttr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
