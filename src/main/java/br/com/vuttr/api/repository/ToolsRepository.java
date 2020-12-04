e64868885a63c3d81e07015bae05e3f7d8c9066d
Savio Carvalho <savio_carvalho@outlook.com>
2020-12-03 23:29:08 -0300

    validando existencia da ferramenta antes do cadastro

--- src/main/java/br/com/vuttr/api/repository/ToolsRepository.java
+++ src/main/java/br/com/vuttr/api/repository/ToolsRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ToolsRepository extends JpaRepository<Tools,Long> {
     Page<Tools> findByTags(String tag, Pageable page);
+    Optional<Tools> findByTitle(String title);
 }
